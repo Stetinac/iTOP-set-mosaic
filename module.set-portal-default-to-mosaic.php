1f49f0d6ac06f32027735381e0e65d40dc3cebb9
Pavel Stetina <63617235+Stetinac@users.noreply.github.com>
2022-11-24 02:38:38 +0100

    Add files to the repository

---
+++ module.set-portal-default-to-mosaic.php
@@ -0,0 +1,48 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'set-portal-default-to-mosaic/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => 'Set Mosaic as the default browse mode',
+		'category' => 'portal',
+		// Setup
+		//
+		'dependencies' => array(	
+		),
+		'mandatory' => false,
+		'visible' => true,
+		// Components
+		//
+		'datamodel' => array(
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => 'https://github.com/Stetinac/iTOP-set-mosaic', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
